49ef20a3eb12d76b101d2ee57ce4c702d0689940
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2023-08-08 03:46:50 +0900

    initial commit

---
+++ Stack-Knowledge-iOS/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Stack-Knowledge-iOS
+//
+//  Created by AnnKangHo on 2023/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
