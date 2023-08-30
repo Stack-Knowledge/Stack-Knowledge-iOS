d3eae245db6a1ef502812d05b8957a13451e112b
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2023-08-30 23:45:42 +0900

    initial commit

--- Stack-Knowledge-iOS/SceneDelegate.swift
+++ Stack-Knowledge-iOS/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
 
-        let mainViewController = ViewController()
+        let mainViewController = LoginVC()
         let navigationController = UINavigationController(rootViewController: mainViewController)
         
         window?.rootViewController = navigationController
---
+++ Stack-Knowledge-iOS/ViewControllers/LoginVC/LoginVC.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  Stack-Knowledge-iOS
+//
+//  Created by AnnKangHo on 2023/08/08.
+//
+
+import UIKit
+
+class LoginVC: BaseVC {
+    
+}
+
