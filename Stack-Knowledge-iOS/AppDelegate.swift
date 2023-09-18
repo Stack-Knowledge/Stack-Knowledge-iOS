39631da388405f679f1552be481a4e86ee7fb856
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2023-09-18 15:06:34 +0900

    customTabBar를 사용하기 위한 코드 작성

--- Stack-Knowledge-iOS/AppDelegate.swift
+++ Stack-Knowledge-iOS/AppDelegate.swift
@@ -13,7 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        if #available(iOS 15, *) {
+            let appearance = UITabBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            UITabBar.appearance().standardAppearance = appearance
+            UITabBar.appearance().scrollEdgeAppearance = appearance
+        }
+        
         return true
     }
 
--- Stack-Knowledge-iOS/SceneDelegate.swift
+++ Stack-Knowledge-iOS/SceneDelegate.swift
@@ -17,10 +17,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
 
-        let mainViewController = LoginVC()
-        let navigationController = UINavigationController(rootViewController: mainViewController)
-        
-        window?.rootViewController = navigationController
+        let mainViewController = CustomTabBarController()
+//        let navigationController = UINavigationController(rootViewController: mainViewController)
+//
+        window?.rootViewController = mainViewController
         window?.makeKeyAndVisible()
     }
 
