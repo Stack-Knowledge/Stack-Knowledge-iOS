a06d044af9616cfb36cac15f209333816b3f5ac2
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2023-09-22 14:16:32 +0900

    CustomStackKnowledgeButton 추가

--- Stack-Knowledge-iOS/Custom/Button/CustomStackKnowledgeButton.swift
+++ Stack-Knowledge-iOS/Custom/Button/CustomStackKnowledgeButton.swift
@@ -5,4 +5,34 @@
 //  Created by AnnKangHo on 2023/09/20.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+class CustomStackKnowledgeButton: UIButton {
+    
+    private let customStackKnowledgeButton = UIButton().then {
+        $0.backgroundColor = UIColor(red: 255, green: 169, blue: 39)
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    init(description: String) {
+        super.init(frame: .zero)
+        setTitle(description, for: .normal)
+        setupView()
+    }
+    
+    private func setupView() {
+        layer.cornerRadius = 10
+        backgroundColor = UIColor(red: 255, green: 169, blue: 39)
+        titleLabel?.font = UIFont(name: "Pretendard-Medium", size: 20)
+    }
+}
