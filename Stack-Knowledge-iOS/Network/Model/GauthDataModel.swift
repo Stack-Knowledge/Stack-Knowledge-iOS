6902fe832da28047c202f33343d0a9d544c7eb16
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2023-11-13 20:10:50 +0900

    NetworkResult

---
+++ Stack-Knowledge-iOS/Network/Model/GauthDataModel.swift
@@ -0,0 +1,8 @@
+//
+//  GauthDataModel.swift
+//  Stack-Knowledge-iOS
+//
+//  Created by AnnKangHo on 2023/11/04.
+//
+
+import Foundation
---
+++ Stack-Knowledge-iOS/Network/Service/PostGauthDataService.swift
@@ -0,0 +1,8 @@
+//
+//  PostGauthDataService.swift
+//  Stack-Knowledge-iOS
+//
+//  Created by AnnKangHo on 2023/11/04.
+//
+
+import Foundation
